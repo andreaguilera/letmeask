1472eb2c9f253d2330723efe535d9a9f08d4d9c2
andreaguilera <andre.aguilera555@gmail.com>
2021-06-23 10:14:13 -0300

    Added Homeand New Room screens and authentication with Google

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
+
+import { AuthContextProvider } from './context/AuthContext'
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>  
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+         <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>   
+    </BrowserRouter>
   );
+
 }
 
 export default App;
